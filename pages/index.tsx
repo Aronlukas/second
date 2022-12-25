67d55a09f2f6eca1c9bc440158702f3239be0f92
Aronlukas <hafner.emil@gmail.com>
2022-12-26 00:52:32 +0100

    Fix-Gap-LandingPage-Buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <div className='h-screen w-screen bgLanding flex justify-center items-center flex-wrap flex-col md:flex-row content-center gap-y-4 md:gap-y-8 gap-x-10'>
-        <h1 className='md:order-1 bg-red md:text-6xl text-4xl w-full text-center uppercase md:mb-20 md:mb-0'>Pauline Häfner</h1>
+        <h1 className='md:order-1 bg-red md:text-6xl text-4xl w-full text-center uppercase md:mb-20 mb-0'>Pauline Häfner</h1>
         <button className="order-3 outline outline-1 text-xl px-3 py-1 w-52 hover:bg-white hover:bg-opacity-50">Atelier</button>
         <button className="order-3 outline outline-1 text-xl px-3 py-1 w-52 hover:bg-white hover:bg-opacity-50">Zweiter </button>
         <button className="order-3 outline outline-1 text-xl px-3 py-1 w-52 hover:bg-white hover:bg-opacity-50">Siebdruck</button>
