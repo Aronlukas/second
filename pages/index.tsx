abb57a6ee38d0a84ca27de9ed3be76c13bc35740
Aronlukas <hafner.emil@gmail.com>
2022-12-26 01:13:08 +0100

    Used Link instead of a

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,18 @@
 
 // const inter = Inter({ subsets: ['latin'] })
 
+import Link from "next/link"
+
 export default function Home() {
   return (
     <>
       <div className='h-screen w-screen bgLanding flex justify-center items-center flex-wrap flex-col md:flex-row content-center gap-y-4 md:gap-y-8 gap-x-10'>
         <h1 className='md:order-1 bg-red md:text-6xl text-4xl w-full text-center uppercase md:mb-20 m-0'>Pauline Häfner</h1>
-        <a href="/Atelier" className="text-center order-3 outline outline-1 text-xl px-3 py-1 w-52 hover:bg-white hover:bg-opacity-50">Atelier</a>
-        <a href="/Siebdruck" className="text-center order-3 outline outline-1 text-xl px-3 py-1 w-52 hover:bg-white hover:bg-opacity-50">Siebdruck</a>
-        <a href="/Requisitenbau" className="text-center order-3 outline outline-1 text-xl px-3 py-1 w-52 hover:bg-white hover:bg-opacity-50">Requisitenbau</a>
+        <Link href="/Atelier" className="text-center order-3 outline outline-1 text-xl px-3 py-1 w-52 hover:bg-white hover:bg-opacity-50">Atelier</Link>
+        <Link href="/Siebdruck" className="text-center order-3 outline outline-1 text-xl px-3 py-1 w-52 hover:bg-white hover:bg-opacity-50">Siebdruck</Link>
+        <Link href="/Requisitenbau" className="text-center order-3 outline outline-1 text-xl px-3 py-1 w-52 hover:bg-white hover:bg-opacity-50">Requisitenbau</Link>
 
-        <a href="/aboutMe" className="
+        <Link href="/aboutMe" className="
         text-center
         w-full text-lg md:mt-16 mt-0
         after:content-[''] after:border-t-2 after:border-t-black after:w-14 after:block after:relative
@@ -22,7 +24,7 @@ export default function Home() {
         hover:after:border-white
         order-2
         md:order-4
-        ">About Me</a>
+        ">About Me</Link>
       </div>
     </>
   )
