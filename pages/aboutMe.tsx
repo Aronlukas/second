95c1ef52c74a94f43ef8a6b2b767ad7b638f6db3
Aronlukas <hafner.emil@gmail.com>
2022-12-26 01:20:55 +0100

    no scroll

--- pages/aboutMe.tsx
+++ pages/aboutMe.tsx
@@ -2,7 +2,7 @@
 export default function AboutMe() {
     return (
         <>
-            <h1>About Me</h1>
+            <h1 className="absolute">About Me</h1>
             <div className="flex justify-center items-center w-screen h-screen">
                 <p className="text-5xl text-cyan-700 ">Wir lieben dich Maria82</p>
             </div>
