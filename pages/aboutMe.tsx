1a5744f5f970fc42be8293102a13fac8d73fc166
Aronlukas <hafner.emil@gmail.com>
2022-12-26 01:19:46 +0100

    centerSecret

--- pages/aboutMe.tsx
+++ pages/aboutMe.tsx
@@ -3,7 +3,7 @@ export default function AboutMe() {
     return (
         <>
             <h1>About Me</h1>
-            <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center w-screen h-screen">
                 <p className="text-5xl text-cyan-700 ">Wir lieben dich Maria82</p>
             </div>
         </>
